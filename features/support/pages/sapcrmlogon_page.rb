31a17c9ac19421f1175ebffc9e2717fac0f6d608
William M Tinnes <william.tinnes@lego.com>
2017-05-15 13:31:13 -0400

    test

---
+++ features/support/pages/sapcrmlogon_page.rb
@@ -0,0 +1,6 @@
+class SAPLogonPage
+  button(:AgentRole, id='Y_IC_AGENT')
+  button(:ManagerRole, id='Y_IC_MANAGER')
+  button(:LoyaltyAdmin, id='Y_IC_LOY_ADM')
+  button(:LoyaltyAgent, id='Y_IC_LOY_AGN')
+end
